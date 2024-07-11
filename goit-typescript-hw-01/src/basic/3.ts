dc53059b30b6d5f4b25d8aba3e24840a868cf932
Marianna Dei <d.marianna@icloud.com>
2024-07-12 00:21:29 +0300

    Added code to task 3

---
+++ goit-typescript-hw-01/src/basic/3.ts
@@ -0,0 +1,2 @@
+let variable: string | number; // змінна, яка може містити або рядок, або число (union type)
+let status: "enable" | "disable"; // змінна, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type)
